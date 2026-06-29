09e89b8dac4cf22bb677a8795ee00c2287c5533b
henrique <henriqueguimaraess2005@gmail.com>
2026-06-29 03:17:47 -0300

    Implement main function to call sample generation

---
+++ trabalho5_CEParvoreB/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void gerarAmostras();
+void criarIndice();
+void gerarResultado();
+
+int main()
+{
+
+    gerarAmostras();
+    criarIndice();
+    gerarResultado();
+
+    return 0;
+}
